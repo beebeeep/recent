f2bb9195d4e31090e54eba780cdac34f7c550466
Migalin Daniil <midanil@microsoft.com>
2015-11-20 23:56:25 +0200

    cleanup of debug messages, added timezone support

--- src/recent.c
+++ src/recent.c
@@ -28,17 +28,24 @@
 #include <fcntl.h>
 #include <pcre.h>
 
+#ifdef DEBUG
+#define printd(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define printd(...) asm("nop")
+#endif
 
 struct {
     pcre *regex;
     pcre_extra *re_extra;
     char *format;
+    int has_tz;
 } TS_FORMAT;
 
+static long int local_gmtoff;
 
 off_t find_newline(char *file, off_t pos, off_t min, off_t max, int direction)
 {
-    fprintf(stderr, "find_newline, min %lu, max %lu, direction %i\n", min, max, direction);
+    printd("find_newline, min %lu, max %lu, direction %i\n", min, max, direction);
     for(;;) {
         if (file[pos] == '\n') return pos;
         if (pos == min || pos == max) return -1;
@@ -50,7 +57,7 @@ void print_line(char *file, off_t pos)
 {
     // off_t end = find_newline(file, pos, 0, 1000, 1);
     write(STDERR_FILENO, &file[pos], 80);
-    fprintf(stderr, "...\n");
+    printd("...\n");
 }
 
 void get_ts_format(char *descr)
@@ -83,7 +90,7 @@ void get_ts_format(char *descr)
         fmt = strsep(&buf, "=");
         if (!strcmp(fmt, descr)) {
             char *re_ptr = strsep(&buf,"|");
-            fprintf(stderr, "regex '%s', fmt '%s'\n", re_ptr, buf);
+            printd("regex '%s', fmt '%s'\n", re_ptr, buf);
             if (buf == NULL) {
                 printf("Error parsing timestamp format: '|' missing\n");
                 exit(3);
@@ -105,6 +112,13 @@ void get_ts_format(char *descr)
                 TS_FORMAT.regex = NULL;
             }
             TS_FORMAT.format = buf;
+            if (strstr(TS_FORMAT.format, "%z") != NULL || strstr(TS_FORMAT.format, "%Z") != NULL) {
+                printd("Taking timezone into account");
+                TS_FORMAT.has_tz = 1;
+            } else {
+                printd("Timezone missing, treat all timestamps as local time");
+                TS_FORMAT.has_tz = 0;
+            }
             return;
         }
         buf = fmt;
@@ -115,7 +129,7 @@ void get_ts_format(char *descr)
 
 time_t get_nearest_timestamp(char *file, off_t *pos, off_t min, off_t max, int direction)
 {
-    fprintf(stderr, "get_nearest_timestamp pos %lu, min %lu, max %lu, dir %i\n", *pos, min, max, direction);
+    printd("get_nearest_timestamp pos %lu, min %lu, max %lu, dir %i\n", *pos, min, max, direction);
     struct tm ts;
     int rc;
     int ovector[30];
@@ -123,16 +137,21 @@ time_t get_nearest_timestamp(char *file, off_t *pos, off_t min, off_t max, int d
     char *rest = NULL;
     memset(&ts, 0, sizeof(ts));
     for(;;) {
-        fprintf(stderr, "Searching nearest direction %i from >>>", direction);
-        print_line(file, *pos);
+        printd("Searching nearest ts, direction %i\n", direction);
         if (TS_FORMAT.regex != NULL) {
             rc = pcre_exec(TS_FORMAT.regex, TS_FORMAT.re_extra, &file[*pos], max - *pos, 0, 0, ovector, 30);
             if (rc > 0) {
                 pcre_copy_substring(&file[*pos], ovector, rc, 1, t, 100);
-                fprintf(stderr, "Matching against '%s'\n", t);
+                printd("Matching against '%s'\n", t);
                 rest = strptime(&file[*pos + ovector[2]], TS_FORMAT.format, &ts);
+                if (TS_FORMAT.has_tz) {
+                    // if timestamp format contains timezone, convert ts to local time as far as mktime expects local time
+                    // otherwise, do nothing, i.e. treat all timestamps in file given in local time
+                    ts.tm_sec = ts.tm_sec - ts.__tm_gmtoff + local_gmtoff;
+                    ts.__tm_gmtoff = local_gmtoff;
+                }
                 strftime(t, 100, "%F %T %z", &ts);
-                fprintf(stderr, "match! ts: %s\n", t);
+                printd("match! ts: %s\n", t);
             } else {
                 rest = NULL;
             }
@@ -164,15 +183,11 @@ int main(int argc, char *argv[])
     char *file;
     time_t seconds;
     int fd;
-    int debug = 0;
     off_t filesize;
     time_t target_timestamp;
 
-    while((c = getopt(argc, argv, "dn:t:")) != -1) {
+    while((c = getopt(argc, argv, "n:t:")) != -1) {
         switch(c) {
-            case 'd':
-                debug = 1;
-                break;
             case 'n':
                 seconds = atol(optarg);
                 if (errno == EINVAL) {
@@ -217,7 +232,10 @@ int main(int argc, char *argv[])
     off_t pivot;
     off_t prev_ts_pos;
     off_t next_ts_pos;
-    target_timestamp = time(NULL) - seconds;
+    time_t now = time(NULL);
+    struct tm *tm_now = localtime(&now);
+    local_gmtoff = tm_now->__tm_gmtoff;
+    target_timestamp = now - seconds;
     unsigned iterations = 0;
 
     for (;;) {
@@ -236,11 +254,12 @@ int main(int argc, char *argv[])
         ctime_r(&next_ts, n); 
         ctime_r(&prev_ts, p);
         ctime_r(&target_timestamp, t);
-        fprintf(stderr, "next %sprev %starget %s\n", n, p, t);
+        //printd("next %sprev %starget %s\n", n, p, t);
+        printd(">> next %lu prev %lu target %lu\n", next_ts, prev_ts, target_timestamp);
 
         if (next_ts == 0 && prev_ts < target_timestamp) {
             /* nothing found */
-            fprintf(stderr, "Nothing found\n");
+            printd("Nothing found\n");
             break;
         }
         if (prev_ts == 0 && next_ts >= target_timestamp ) {
@@ -261,9 +280,8 @@ int main(int argc, char *argv[])
             chunk_end = pivot;
         }
     }
-    if (debug) {
-        fprintf(stderr, " ====> done in %u iterations\n", iterations); 
-    }
+
+    printd(" ====> done in %u iterations\n", iterations);
 
     munmap(file, 0);
     close(fd);
